56cf3f70817e552f823e1ac3219b9ddfe4b80a8b
Romildo Jozue Paiter <romildopaiter@gmail.com>
2021-03-28 19:14:41 -0300

    Retirando  divisão

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -15,7 +15,7 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: MediaQuery.of(context).size.height / 2,
+      height: MediaQuery.of(context).size.height,
       child: transactions.isEmpty
           ? Column(
               children: [
